28c2c8d733f56b46cd2383d6c525fbdb18af74e4
Guilherme Nascimento <guilherme_cln@hotmail.com>
2023-02-08 04:14:59 -0300

    baseline + RandomizedSearchCV

--- tools_ml/models.py
+++ tools_ml/models.py
@@ -5,25 +5,25 @@ from sklearn.impute import SimpleImputer
 
 
 
-def get_pipeline(features_num: list, features_cat: list, classifier):
-    pipe_tr_features_num = Pipeline([
+def get_pipeline(num_features: list, cat_features: list, estimator):
+    pipe_tr_num_features = Pipeline([
         ('tr_imput_mean', SimpleImputer(strategy='mean')),
         ('tr_min_max', StandardScaler())
     ])
 
-    pipe_tr_features_cat = Pipeline([
+    pipe_tr_cat_features = Pipeline([
         ('tr_input_frequent', SimpleImputer(strategy='most_frequent')),
         ('tr_dummy', OneHotEncoder(drop='first', handle_unknown='ignore'))
     ])
 
     pre_processor = ColumnTransformer([
-        ('tr_num', pipe_tr_features_num, features_num),
-        ('tr_cat', pipe_tr_features_cat, features_cat)
+        ('tr_num', pipe_tr_num_features, num_features),
+        ('tr_cat', pipe_tr_cat_features, cat_features)
     ])
 
     final_pipe = Pipeline([
         ('pre_processor', pre_processor),
-        ('classifier', classifier)
+        ('estimator', estimator)
     ])
 
     return final_pipe
